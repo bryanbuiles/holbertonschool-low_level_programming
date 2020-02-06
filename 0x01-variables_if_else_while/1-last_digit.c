1e65f2a1e118dfdb2c908c2e93fcf5704dccc2e0
Bryan Builes <1482@holbertonschool.com>
2020-02-06 19:18:04 +0000

    2 task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ printf("Last digit of %d is %d and is 0\n", n, l);
 }
 else if (l < 6)
 {
-printf("Last digit of %d is %d and is less than 6\n", n, l);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 }
 return (0);
 }
