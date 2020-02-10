4f298e35b0fb9c05ca0020977a3794e1af6c4d56
Bryan Builes <1482@holbertonschool.com>
2020-02-10 23:41:42 +0000

    3 task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_alphabet - entry point
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+int lo;
+int i;
+ 
+for (i = 0 ; i < 10 ; i++)
+{
+for (lo = 'a' ; lo <= 'z' ; lo++)
+{
+_putchar(lo);
+}
+_putchar('\n');
+}
+}
