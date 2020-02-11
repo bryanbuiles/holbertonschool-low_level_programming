4321cec29cdc426368bbc97fde3954e64f0471fb
Bryan Builes <1482@holbertonschool.com>
2020-02-11 00:19:50 +0000

    3 task

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+* _islower : check lowercase
+*
+* Return: 1 is lowercase or 0 if not
+*/
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else 
+return (0);
+}
