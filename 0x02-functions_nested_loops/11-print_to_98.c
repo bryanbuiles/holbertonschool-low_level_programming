daa0de0afd87f843b39a3cc02cef7cbc323dee36
Bryan Builes <1482@holbertonschool.com>
2020-02-11 21:53:34 +0000

    final task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_to_98 - entry point
+ * @n: Parameter
+ *
+ * Return:  (success)
+ */
+void print_to_98(int n)
+{
+if (n > 98)
+for (n = 0 ; n > 98 ; n--)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
+}
+else if (n < 98)
+{
+for (n = 0 ; n <= 98 ; n++)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
+}
+}
+else 
+{
+printf("98");
+}
+printf("\n");
+}
