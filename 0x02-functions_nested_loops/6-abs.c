64cf31ab46b40184563baa1527bddd90a55048c6
Bryan Builes <1482@holbertonschool.com>
2020-02-11 17:13:11 +0000

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <math.h>
+/**
+  * abs - less  more or zero
+  * @n: parameter
+  * Return: 1 is lowercase or 0 if not
+  */
+int _abs(int n) 
+{
+	if (n >= 0)
+	return (n);
+	else
+	return ( n = n*-1);
+}
