4e341311c9cc9cc6fc83260117e48ad139627d9d
Bryan Builes <1482@holbertonschool.com>
2020-02-11 20:32:57 +0000

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* add - less  more or zero
+* @a: first operand
+* @b: second operand
+*
+* Return: n is the add
+*/
+int add(int a, int b)
+{
+int n;
+n = (a + b);
+
+return (n);
+}
