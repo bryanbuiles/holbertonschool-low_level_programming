667820934ad6f2103be6c21dd2dc600f78529e06
Bryan Builes <1482@holbertonschool.com>
2020-02-12 22:04:55 +0000

    11 task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,18 +8,9 @@
  */
 void print_to_98(int n)
 {
-if (n > 98)
-for (n = 0 ; n > 98 ; n--)
+if (n < 98)
 {
-printf("%d", n);
-if (n != 98)
-{
-printf(", ");
-}
-}
-else if (n < 98)
-{
-for (n = 0 ; n <= 98 ; n++)
+for (n = n ; n <= 98 ; n++)
 {
 printf("%d", n);
 if (n != 98)
@@ -28,7 +19,18 @@ printf(", ");
 }
 }
 }
-else 
+else if (n  > 98)
+{
+for (n = n ; n >= 98 ; n--)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
+}
+}
+else
 {
 printf("98");
 }
