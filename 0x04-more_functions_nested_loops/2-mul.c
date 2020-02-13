5b49ff2663601baa3b19035619297b267a9cb62d
Bryan Builes <1482@holbertonschool.com>
2020-02-13 16:19:49 +0000

    3 task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+  * mul - check lowercase
+  * @a: first operand
+  * @b: second operand
+  * Return: mult
+  */
+int mul(int a, int b)
+{
+int n;
+n = (a * b);
+return (n);
+}
