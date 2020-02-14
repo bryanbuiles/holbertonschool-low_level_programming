c1e61aaea6f060c2a441a8cce2dc94173612cb29
bryanbuiles <1482@holbertonschool.com>
2020-02-13 19:00:58 -0500

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,22 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * more_numbers - entry point
+ * main - entry point
  *
  * Return: Always 0 (success)
  */
 int main(void)
+
 {
 int i;
-
 for (i = 1 ; i <= 100 ; i++)
 {
-  if (!((i % 3) == 0 ) && (!((i % 5) == 0)))
-printf("%d", i);	
-	if ( (i % 3) == 0)		
-printf("Buzz");
+if (!((i % 3) == 0) && (!((i % 5) == 0)))
+printf("%d ", i);
+if (((i % 3) == 0) && ((i % 5) == 0))
+printf("FizzBuzz ");
+else if ((i % 3) == 0)
+printf("Fizz ");
+else if ((i % 5) == 0)
+printf("Buzz ");
 }
-{
-	printf("\n");
-			}
+printf("\n");
 return (0);
 }
