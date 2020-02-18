d7bf031ad8bb5c38257679a1bacba6288599a23e
Bryan Builes <1482@holbertonschool.com>
2020-02-18 15:37:11 +0000

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_rev - entry point
+ * @s: pointer
+ * Return: Always 0 (success)
+ */
+
+void print_rev(char *s)
+{
+int n,j,i;
+j=0;
+for (n =0 ; s[n] != '\0'; n++)
+{
+j+=1;
+}
+for (i=(j-1); i >= 0 ; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
