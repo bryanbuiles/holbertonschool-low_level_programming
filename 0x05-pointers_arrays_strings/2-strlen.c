017766e6ba1ba06509253d132759c8f611e081d3
Bryan Builes <1482@holbertonschool.com>
2020-02-18 13:59:02 +0000

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,15 @@
-#include "holberton.h"
+B#include "holberton.h"
 
 /**
- * reset_to_98 - entry point
- * @n : pointer
+ * _strlen - entry point
+ * @s : pointer
  * Return: Always 0 (success)
  */
 
 int _strlen(char *s)
 {
 
-int i;	
+int i;
 for (i = 0; s[i] != '\0'; ++i)
 {
 return (i);
