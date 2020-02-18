6b0d09fa94809585e249193302aa341218f1d024
Bryan Builes <1482@holbertonschool.com>
2020-02-18 23:57:56 +0000

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,10 +9,19 @@
 char *_strcpy(char *dest, char *src)
 {
 	int n = 0;
+	int sum = 0;
+	int j;
 
 	for (; src[n] != '\0'; n++)
 	{
-		dest[n] = src[n];
+		sum = sum + 1;
 	}
+	for (j = 0; j < sum; j++)
+	{
+	dest[j] = src[j];
+	}
+	dest[sum] = '\0';
 	return (dest);
+	return (src);
 }
+
