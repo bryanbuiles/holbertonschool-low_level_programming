3bd8e715a085314773d99adaf73d6873d56c466d
Bryan Builes <1482@holbertonschool.com>
2020-02-26 16:26:20 +0000

    first task

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - entry point
+ * @s: pointer
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+	int n = 0;
+
+	_putchar(s[n]);
+
+	if (s[n] == '\0')
+	{
+		_putchar('\0');
+		_putchar('\n');
+	}
+	else
+	{
+		_puts_recursion(s + (n + 1));
+	}
+}
+
+
