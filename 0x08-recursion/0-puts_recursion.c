50a9c2eb8f3934053eaa71ffd2696b46517888dc
Bryan Builes <1482@holbertonschool.com>
2020-02-26 17:56:33 +0000

    first task

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,15 +10,15 @@ void _puts_recursion(char *s)
 {
 	int n = 0;
 
-	_putchar(s[n]);
-
-	if (s[n] == '\0')
+	if (s[n] ==  '\0')
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		_puts_recursion(s + (n + 1));
+		_putchar(s[n]);
+		_puts_recursion(s + 1);
+
 	}
 }
 
