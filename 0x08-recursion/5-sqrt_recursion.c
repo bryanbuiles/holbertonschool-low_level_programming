563cc54bad02da6700506fb4030e291e0c824dbf
bryanbuiles <1482@holbertonschool.com>
2020-02-27 00:00:41 -0500

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,36 +1,29 @@
 #include "holberton.h"
-int _squarefunction(int o, int p);
+int squareroot(int n, int i);
 /**
- * _sqrt_recursion - entry point
- * @n: Number
- * Return: n
+ * _sqrt_recursion - no loop, 1. 2nd function - checks for perfect square
+ * @n: input
+ * Return: Always 0 (Success)
  */
 int _sqrt_recursion(int n)
 {
-if (n < 0)
-{
-return (-1);
-}
-{
-return (_squarefunction(n, (n + 1) / 2));
-}
+	if (n < 0)
+		return (-1);
+	else
+		return (squareroot(n, (n + 1) / 2));
 }
 /**
- * _squarefunction - second function
- * @o: variable
- * @p: second variable
- * Return: square root of a number
+ * squareroot - checks if perfect square
+ * @n: input
+ * @i: counter
+ * Return: if square root
  */
-int _squarefunction(int o, int p)
+int squareroot(int n, int i)
 {
-if (p < 1)
-return (-1);
-else if (p * p == o)
-{
-return (p);
-}
-else
-{
-return (_squarefunction(o, p - 1));
-}
+	if (i < 1)
+		return (-1);
+	else if (i * i == n)
+		return (i);
+	else
+		return (squareroot(n, i - 1));
 }
