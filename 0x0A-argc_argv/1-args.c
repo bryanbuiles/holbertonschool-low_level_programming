3c25a6ecf71b94952573870e5e8ee732259de20e
Bryan Builes <1482@holbertonschool.com>
2020-02-28 21:42:16 +0000

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: number of elements
+ * @argv: Array of arguments
+ * Return: 0
+ */
+
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	{
+	printf("%d\n", argc - 1);
+	}
+		return (0);
+}
