3aa2fe76aa9e5881ae831ac2db22cc5e153bd1e0
Bryan Builes <1482@holbertonschool.com>
2020-02-28 20:01:59 +0000

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - entry point
+ * @s: pointer
+ * Return: 0
+ */
+
+int main (int argc, char *argv[])
+{
+
+	if (argc >= 1)
+{
+	printf("%s\n", argv[0]);
+}
+return (0);
+}
