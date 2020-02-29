f02268102c3a7e4a64876313d9352cbf806fbc52
bryanbuiles <1482@holbertonschool.com>
2020-02-29 17:05:26 -0500

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,27 +2,28 @@
 #include <stdlib.h>
 #include "holberton.h"
 /**
-  * main - main function
-  * @argc: Number of arguments
-  * @argv: Array of arguments
-  * Return: 0
-  */
-
+ * main - Entry Point
+ * @argc: arguments
+ * @argv: array pointing to arguments
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
-int i, j = 0;
+	int i, sum = 0;
 
-if (argc < 1)
-return (0);
-for (i = 1; i < argc ; i++)
-{
-if (!atoi(argv[i]))
-{
-printf("%s\n", "Error");
-return (1);
-}
-j = j + atoi(argv[i]);
-}
-printf("%d\n", j);
-return (0);
+	if (argc < 1)
+		return (0);
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
 }
