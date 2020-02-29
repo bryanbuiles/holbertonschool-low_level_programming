c21c8b6666d052105c33fba0c2a44ef682f0539a
bryanbuiles <1482@holbertonschool.com>
2020-02-29 17:28:39 -0500

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,6 +13,8 @@ int i, sum = 0;
 
 if (argc < 1)
 printf("0\n");
+else if (argc > 1)
+{
 for (i = 1; i < argc; i++)
 {
 if (!atoi(argv[i]))
@@ -21,6 +23,7 @@ printf("%s\n", "Error");
 return (1);
 }
 sum += atoi(argv[i]);
+}
 }
 printf("%d\n", sum);
 return (0);
