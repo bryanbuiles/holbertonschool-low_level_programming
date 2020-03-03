c6936c3a790a0ed2dd0c3cdec7ac8d99056f39e5
Bryan Builes <1482@holbertonschool.com>
2020-03-03 21:47:32 +0000

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+  * *str_concat - entry point
+  * @s1: pointer
+  * @s2: pointer
+  * Return: p (success)
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+
+	int i;
+	char *p;
+	int j;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		s1 = "";
+		s2 = "";
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+	}
+	p = malloc(sizeof(char) * (i + j + 1));
+			if (p == NULL)
+			return (NULL);
+			for (i = 0; s1[i] != '\0'; i++)
+			p[i] = s1[i];
+			for (j = 0; s2[j] != '\0'; j++)
+			{
+			p[i] = s2[j];
+			i++;
+			}
+			return (p);
+}
