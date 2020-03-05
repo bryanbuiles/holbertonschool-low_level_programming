26c066ad4bdd23c04d1c6d871e19543b1d98603e
Bryan Builes <1482@holbertonschool.com>
2020-03-05 21:04:59 +0000

    task 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * *array_range - entry point
+ * @min: Min of elements
+ * @max: Max of elements
+ * Return: malloc (success)
+*/
+
+int *array_range(int min, int max)
+{
+	int *a;
+	int i;
+	int s = 0;
+
+	if (min > max)
+		return (NULL);
+	a = malloc(sizeof(int) * (max - min) + 1);
+
+	if (a == 0)
+		return (NULL);
+		for (i = min; i <= max; i++)
+	{
+		a[s] = i;
+		s++;
+	}
+	return (a);
+}
+
