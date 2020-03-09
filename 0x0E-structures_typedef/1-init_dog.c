5488300a66749fd5bf3805332853a21044358d59
Bryan Builes <1482@holbertonschool.com>
2020-03-09 22:26:11 +0000

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - entry point
+ * @d: pointer
+ * @name: DOg name
+ * @age: Dog age
+ * @owner: Dog owner
+ * Return: Void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
