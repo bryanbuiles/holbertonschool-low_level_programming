a9ee8904aa2719c43525fecc6863a332246892e6
Bryan Builes <1482@holbertonschool.com>
2020-03-09 22:37:42 +0000

    task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,22 @@
 #ifndef DOG
 #define DOG
+
 /**
   * struct dog - main struct
   * @name: Dog name
   * @owner: Dog Owner
   * @age: Dog age
   * Description: My dog
-  */
+ */
 
 struct dog
 {
-char *name;
-char *owner;
-float age;
+	char *name;
+	char *owner;
+	float age;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
 #endif
