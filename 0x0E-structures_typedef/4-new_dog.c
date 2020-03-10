9a8ce690dc7bfc1695b9b62bb8ebae1726e62eaf
Bryan Builes <1482@holbertonschool.com>
2020-03-10 01:00:46 +0000

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- * print_dog - entry point
- * @d: pointer
- * Return: Void
+ * _strdup - main function
+ * @str: Number of arguments
+ * Return: p
  */
 
 char *_strdup(char *str)
@@ -29,26 +29,35 @@ char *_strdup(char *str)
 	return (p);
 }
 
+/**
+  * new_dog - entry point
+  * @name: DOg name
+  * @age: Dog age
+  * @owner: Dog owner
+  * Return: Void
+ */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog_t;
+
 	newdog_t = malloc(sizeof(dog_t));
 	if (newdog_t == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
 	newdog_t->name = _strdup(name);
 	if (newdog_t->name == NULL)
 	{
-		free (newdog_t);
+		free(newdog_t);
 		return (NULL);
 	}
 	newdog_t->age = age;
 	newdog_t->owner = _strdup(owner);
 	if (newdog_t->owner == NULL)
 	{
-		free (newdog_t->name);
-		free (newdog_t);
+		free(newdog_t->name);
+		free(newdog_t);
 		return (NULL);
 	}
 	return (newdog_t);
