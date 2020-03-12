bdc77f771bc1d4ace4bbee86a62bb9bb327f1010
bryanbuiles <1482@holbertonschool.com>
2020-03-11 19:03:06 -0500

    task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - entry function
+ * @argc: OPerator
+ * @argv: NUmber
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, valor;
+	int (*resul)(int, int);
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	if (argc > 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((get_op_func(argv[2])) == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*argv[2] == '/' || *argv[2] == '%') && (*argv[3] == '0'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	resul = get_op_func(argv[2]);
+	valor = resul(num1, num2);
+	printf("%d\n", valor);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * op_add - sum operation
+ * @a: number
+ * @b: number
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - sub operation
+ * @a: number
+ * @b: number
+ * Return: sub
+ */
+
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - mul operation
+ * @a: number
+ * @b: number
+ * Return: mul
+ */
+
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - mul operation
+ * @a: number
+ * @b: number
+ * Return: div
+ */
+
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_mod - mul operation
+ * @a: number
+ * @b: number
+ * Return: mod
+ */
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}
