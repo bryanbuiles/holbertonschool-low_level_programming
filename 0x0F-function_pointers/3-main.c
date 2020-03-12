8af1c6d30733fbbc63f4c615ec862c03cf907e17
bryanbuiles <1482@holbertonschool.com>
2020-03-11 19:41:54 -0500

    task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,14 +17,14 @@ int main(int argc, char *argv[])
 	if (argc != 4)
 	{
 		printf("Error\n");
-		return(98);
+		return (98);
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	if ((get_op_func(argv[2])) == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
-		return(99);
+		return (99);
 	}
 	if ((*argv[2] == '/' || *argv[2] == '%') && (*argv[3] == '0'))
 	{
