7180dc473fa40e1ed982571740c8e6b510116ee2
bryanbuiles <1482@holbertonschool.com>
2020-03-13 21:21:13 -0500

    task finished

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,54 +2,51 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdlib.h>
-
 /**
- * print_strings - Funtion that prints strings.
- * @n: number of elements
- * @separator: separator
+ * print_all - Funtion that prints anything.
+ * @format: is a list of types of arguments passed to the function
  * Return: void.
  */
-
 void print_all(const char * const format, ...)
 {
-{
-	
-    
-    
-    
-    
-    
-    
-    
-    
-    va_list valist;
-	unsigned int i = 0;
+	va_list valist;
+	int i = 0, n = 0;
 	char *j;
+	char *coma = ", ";
 
-	if (separator != NULL)
+	va_start(valist, format);
+	while (format && format[i])
+		i++;
+	while (format && format[n])
 	{
-		va_start(valist, n);
-		for (i = 0; i < n; i++)
+		if (n == (i - 1))
+			coma = "";
+		switch (format[n])
 		{
-			j = va_arg(valist, char*);
-			if (j != NULL)
-			{
+			case 'c':
+				printf("%c", va_arg(valist, int));
+				printf("%s", coma);
+				break;
+			case 'i':
+				printf("%d", va_arg(valist, int));
+				printf("%s", coma);
+				break;
+			case 'f':
+				printf("%f", va_arg(valist, double));
+				printf("%s", coma);
+				break;
+			case 's':
+				j = va_arg(valist, char *);
+				if (j == NULL)
+				{
+					printf("nil");
+				}
 				printf("%s", j);
+				printf("%s", coma);
+				break;
 			}
-			else
-			{
-				printf("nil");
-			}
-			if (i < n - 1)
-				printf("%s", separator);
-		}
+			n++;
+	}
 	printf("\n");
 	va_end(valist);
-	}
-}
-
-
-
-
-
 }
