67b7352b95a2d479608342a52e33f485cf2fdc8e
bryanbuiles <1482@holbertonschool.com>
2020-03-31 14:23:15 -0500

    first push

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "holberton.h"
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard out
+ * @filename: Name of the file to read
+ * @letters: number of letters to print
+ * Return: The number of nodes
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int rd, fd, wr;
+	char buf[letters + 1];
+
+	if (filename == NULL)
+		return (0);
+	rd = open(filename, O_RDONLY);
+	if (rd == -1)
+		return (0);
+	fd = read(rd, buf, letters);
+	if (fd == -1)
+		return (0);
+	buf[letters] = '\0';
+	wr = write(1, buf, fd);
+	if (wr == -1)
+		return (0);
+	close(rd);
+	return (wr);
+}
