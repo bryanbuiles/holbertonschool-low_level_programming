37d85d524bb2c1b2509355ccee418f408115a11b
bryanbuiles <1482@holbertonschool.com>
2020-05-03 10:55:48 -0500

    last tasks fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,7 +10,7 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current, *new;
+	dlistint_t *current = NULL, *new = NULL;
 	unsigned int i = 1;
 
 	new = malloc(sizeof(dlistint_t));
