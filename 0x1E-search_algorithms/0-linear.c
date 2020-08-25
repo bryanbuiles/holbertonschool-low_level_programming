982f66aba4eee37cc1190c21efa738681a1103d3
bryanbuiles <1482@holbertonschool.com>
2020-08-25 15:20:27 -0500

    first tasks

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+  * linear_search - entry point
+  * @array: pointer to the first element of the array
+  * @size: is the number of elements in array
+  * @value: is the value to search for
+  * Return: int (success)
+  */
+
+int linear_search(int *array, size_t size, int value)
+{
+	int i, j;
+
+	j = size - 1;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i <= j; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+  * binary_search - entry point
+  * @array: pointer to the first element of the array
+  * @size: is the number of elements in array
+  * @value: is the value to search for
+  * Return: int (success)
+  */
+
+int binary_search(int *array, size_t size, int value)
+{
+	int inferior, superior, m, i, j;
+
+	inferior = 0;
+	superior = size - 1;
+
+	if (array == NULL)
+		return (-1);
+
+	while (inferior <= superior)
+	{
+		printf("Searching in array: ");
+		j = superior;
+		for (i = inferior; i <= j; i++)
+		{
+			if (i == j)
+				printf("%d\n", array[i]);
+			else
+				printf("%d, ", array[i]);
+		}
+		m = (superior + inferior) / 2;
+		if (array[m] == value)
+			return (m);
+		/* es mayor que elvalor */
+		if (array[m] > value)
+			superior = (m - 1);
+		else if (array[m] < value)
+			inferior = m + 1;
+	}
+	return (-1);
+}
