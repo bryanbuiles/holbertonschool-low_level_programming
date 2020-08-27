6e04ccae8391ff4633166683a59581e21e3f6858
bryanbuiles <1482@holbertonschool.com>
2020-08-27 11:42:59 -0500

    task 104

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,46 @@
+#include "search_algos.h"
+
+/**
+  * advanced_binary - advance binary search
+  * @array: pointer to the first element of the array
+  * @size: is the number of elements in array
+  * @value: is the value to search for
+  * Return: int (success)
+  */
+
+int advanced_binary(int *array, size_t size, int value)
+{
+	int inferior, superior, m, i, j;
+
+	inferior = 0;
+	superior = size - 1;
+
+	if (array == NULL && size == 0)
+		return (-1);
+
+	while (inferior <= superior)
+	{
+		printf("Searching in array: ");
+		j = superior;
+		for (i = inferior; i <= j; i++)
+		{
+			if (i == j)
+				printf("%d\n", array[i]);
+			else
+				printf("%d, ", array[i]);
+		}
+		m = (superior + inferior) / 2;
+		if (array[m - 1] == value)
+			superior = m;
+		else
+		{
+			if (array[m] == value)
+				return (m);
+			if (array[m] < value)
+				inferior = m + 1;
+			else if (array[m] > value)
+				superior = (m - 1);
+		}
+	}
+	return (-1);
+}
