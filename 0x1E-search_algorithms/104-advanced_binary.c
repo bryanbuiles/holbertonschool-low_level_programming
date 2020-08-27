3d1ee4323aafb0880efb812b16d4e4c878e1eb45
bryanbuiles <1482@holbertonschool.com>
2020-08-27 14:54:54 -0500

    fix 0 in index 0

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -31,6 +31,8 @@ int advanced_binary2(int *array, size_t left, size_t right, int value)
 	int i, j;
 	size_t m;
 
+	if (array[0] == 0 && right == 0)
+		return (0);
 	if (left > right)
 		return (-1);
 
@@ -45,7 +47,7 @@ int advanced_binary2(int *array, size_t left, size_t right, int value)
 			printf("%d, ", array[i]);
 	}
 	m = (right + left) / 2;
-	if (array[m - 1] == value && array[m] == value)
+	if (array[m - 1] == value)
 		return (advanced_binary2(array, left, m, value));
 	if (array[m] == value)
 		return (m);
